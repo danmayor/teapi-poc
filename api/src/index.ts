40e642f592dccb8ef93a3637507428111f4844dc
Dan Mayor <dmayor@digivance.com>
2025-03-15 09:44:20 -0400

    introducing data layer

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import { Author, Book } from '@teapi-poc/data';
 
 const app = express();
 const port = 3000;
@@ -9,4 +10,18 @@ app.get('/', async (req: Request, res: Response) => {
 
 app.listen(port, () => {
     console.log(`We're online at http://localhost:${port}`);
+
+    const author: Author = {
+        id: 1,
+        name: 'Dan Mayor'
+    };
+
+    const book: Book = {
+        author,
+        description: 'Best book ever written',
+        id: 1,
+        title: 'BestBook'
+    };
+
+    console.log('Come see our book!', book);
 });
